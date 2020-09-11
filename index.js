2a4ca7b087dc3286eb2b94a513bbb9b45473c673
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-09-11 00:08:12 -0300

    Funcionalidade: Adicionadas funções cadastrarMedico, que adiciona um novo médico à lista, listarMedicos, que retorna a lista completa de médicos, obterMedico, que retorna o médico de acordo com o ID, atualizarMedico, que atualiza os dados do médico com base no valor do input e removerMedico que remove o registro do médico.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyparser = require("koa-bodyparser");
 const app = new Koa();
 app.use(bodyparser());
 
+const medicos = [];
+
 const sucessoRequisicao = (ctx, codigoREST, conteudo) => {
     ctx.status = codigoREST;
     ctx.body = {
@@ -20,6 +22,72 @@ const falhaRequisicao = (ctx, codigoREST, mensagem) => {
         }
     }
 }
+
+const cadastrarMedico = (especialidade,nome = '') => {
+    if (!especialidade || especialidade.trim() === '' || especialidade === null) {
+        console.log('400 - Adicione pelo menos uma especialidade para cadastrar um médico.')
+        return false;
+    } else {
+        let indexMedico;
+        (medicos.length === 0) ? indexMedico = 1 : indexMedico = medicos.length + 1;
+        const novoMedico = {
+            id : indexMedico,
+            nome: nome,
+            especialidade: especialidade,
+        }
+        medicos.push(novoMedico);
+        return novoMedico;
+    }
+}
+
+const listarMedicos = () => {
+    return medicos;
+}
+
+const obterMedico = (id) => {
+    for (medico of medicos) {
+        if (medicos[id-1] === medico) return medico; 
+    }
+    console.log('404 - ID não encontrado.');
+    return false
+}
+
+const atualizarMedico = (id, medicoJSON) => {
+    if (medicoJSON.id) {
+        console.log('403 - Não é possível alterar o valor de ID. Insira os dados do médico sem o ID.')
+        return false;
+    } else if (!medicoJSON.especialidade || !medicoJSON.especialidade.trim() === '' || !medicoJSON.especialidade === null) {
+        console.log('400 - Insira uma propriedade válida para atualização.');
+        return false;
+    } else {
+        if (!obterMedico(id)) {
+            console.log('404 - ID não encontrado.');
+            return false;
+        } else {
+            const index = medicos.indexOf(obterMedico(id));
+            medicos[index] = {
+                id: medicos[index].id,
+                nome: medicoJSON.nome,
+                especialidade: medicoJSON.especialidade,
+            }
+            return medicos[index]
+        }
+    }
+}
+
+const removerMedico = (id) => {
+    if (!obterMedico(id)) {
+        console.log('404 - ID não encontrado.');
+        return false;
+    } else {
+        const index = medicos.indexOf(obterMedico(id));
+        medicos.splice(index, 1);
+    
+        return medicos
+    }
+}
+
+
 
 app.use((ctx) => {
 
